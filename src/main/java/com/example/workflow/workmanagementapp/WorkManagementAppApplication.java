24500bc339fa5f0b502aa1b325d7ec167f8de693
Kenneth Stallworth <kennethstallworth@hotmail.com>
2025-08-03 11:44:12 -0400

    initial commit

---
+++ src/main/java/com/example/workflow/workmanagementapp/WorkManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.workflow.workmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkManagementAppApplication.class, args);
+	}
+
+}
